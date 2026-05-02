cae2d294cc8f1138a2b6ba9396d98b580df2d751
J-iyu-N <mitoohjy@gmail.com>
2026-05-02 09:51:53 +0900

    체력 증감 프리팹 제작

---
+++ Assets/Script/Generator/TextUIGenerator.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TextUIGenerator : MonoBehaviour
+{
+    public void SpawnText()
+    {
+        // 여기서 판단해서 아래 매서드 호출
+    }
+    public void SpawnDamageText()
+    {
+        
+    }
+    public void SpawnHealText()
+    {
+        
+    }
+}
